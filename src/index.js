74ca3dac728029d07fc9fb9540042a8ad2ccb2a2
Jayden Windle <jaydenwindle@gmail.com>
2019-11-22 13:39:55 -0600

    added file upload

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import { Client } from "rpc-websockets";
 
 export default class LotusClient {
-  constructor(client) {
+  constructor(client, host, port, token) {
     this.client = client;
+    this.host = host;
+    this.port = port;
+    this.token = token;
   }
 
   static async init({ host, port = 1234, token }) {
@@ -10,7 +13,7 @@ export default class LotusClient {
       var ws = new Client(`ws://${host}:${port}/rpc/v0?token=${token}`, {
         protocol: "JSONRPC"
       });
-      ws.on("open", () => resolve(new LotusClient(ws)));
+      ws.on("open", () => resolve(new LotusClient(ws, host, port, token)));
       ws.on("error", err => reject(err));
     });
   }
@@ -37,6 +40,16 @@ export default class LotusClient {
 
   async chainHead() {
     return this.client.call("Filecoin.ChainHead");
+  }
+
+  async importFile(file) {
+    return fetch(`http://${this.host}:${this.port}/rest/v0/import`, {
+      method: "PUT",
+      headers: {
+        Authorization: `Bearer ${this.token}`
+      },
+      body: file
+    });
   }
 
   close() {

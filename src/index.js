8e7b5c354080e6938526666f78691a4bfd780080
Jayden Windle <jaydenwindle@gmail.com>
2019-11-22 11:37:36 -0600

    initial library definition

--- src/index.js
+++ src/index.js
@@ -1,13 +1,45 @@
-import "./index.css";
+import { Client } from "rpc-websockets";
 
-class MyLibrary {
-  constructor() {
-    console.log("Library constructor loaded");
+export default class LotusClient {
+  constructor(client) {
+    this.client = client;
   }
 
-  myMethod = () => {
-    console.log("Library method fired");
-  };
-}
+  static async init({ host, port = 1234, token }) {
+    return new Promise((resolve, reject) => {
+      var ws = new Client(`ws://${host}:${port}/rpc/v0?token=${token}`, {
+        protocol: "JSONRPC"
+      });
+      ws.on("open", () => resolve(new LotusClient(ws)));
+      ws.on("error", err => reject(err));
+    });
+  }
 
-export default MyLibrary;
+  async version() {
+    return this.client.call("Filecoin.Version", []);
+  }
+
+  async listImports() {
+    return this.client.call("Filecoin.ClientListImports");
+  }
+
+  async listDeals() {
+    return this.client.call("Filecoin.ClientListDeals");
+  }
+
+  async walletList() {
+    return this.client.call("Filecoin.WalletList");
+  }
+
+  async walletBalance(wallet) {
+    return this.client.call("Filecoin.WalletBalance", [wallet]);
+  }
+
+  async chainHead() {
+    return this.client.call("Filecoin.ChainHead");
+  }
+
+  close() {
+    return this.client.close();
+  }
+}
